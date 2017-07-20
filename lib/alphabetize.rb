7ba33526b4973568067887397d1af86236abfedc
sophiehelf <sophie.helf@gmail.com>
2017-07-19 20:29:54 -0700

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+	chars = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+	chars_array = chars.split('')
+	arr.sort_by do |word|
+		word.split("").map do |x| 
+			chars_array.index(x)
+		end
+	end
 end
